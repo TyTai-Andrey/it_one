9fa8df6b4cc579ecaf64a7408404ed5a500e97fa
Andrey Andriynov <a.andriynov@pa-resultant.ru>
2024-01-19 13:36:42 +0300

    add server

---
+++ client/.vscode/cch-template/React Redux/{{camelCase}}/interfaces.ts
@@ -0,0 +1,9 @@
+import { IParametersForRequestingLists } from '@typings/swagger';
+
+export interface Get{{pascalCase}}Filter extends IParametersForRequestingLists {
+    
+}
+
+export type {{pascalCase}}ItemModel = {
+    
+}
--- client/src/pages/SecondTask/SecondTask.tsx
+++ client/src/pages/SecondTask/SecondTask.tsx
@@ -1,9 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import styles from './SecondTask.module.scss';
 
 export type SecondTaskProps = {};
 
 const SecondTask: FC<SecondTaskProps> = (props) => {
+  useEffect(() => {
+    //check
+    fetch('http://localhost:7070/api/services');
+  }, []);
+
   return (
     <div className={styles.root} {...props}>
       SecondTask component is mounted!
